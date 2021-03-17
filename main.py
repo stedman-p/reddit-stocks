67b0bfdaafbb764c1ccadcbea52f30ffefea280a
Frank Mularcik <mularcikfrank@gmail.com>
2021-03-17 10:21:31 -0400

    Refactoring

--- main.py
+++ main.py
@@ -7,82 +7,97 @@ from selenium.webdriver.chrome.options import Options
 
 tags = []
 urls = {}
+line = 0
 
-def do(event, tag):
-    
+def open_url(event, tag):
     webbrowser.open(urls[tag])
-    
+
+def add_link(event, post):
+    tag = "tag" + str(line)
+    urls[tag] = post.url
+    text.insert('1.0', post.title + "\n")
+    text.tag_add(tag, '1.0', '1.end')
+    text.tag_config(tag, foreground='blue', underline=True)
+    callback = lambda even, tag = tag: open_url(event, tag)
+    text.tag_bind(tag, '<Button-1>', callback)
+    return line + 1
+
+def setup_chrome():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    return webdriver.Chrome(options=chrome_options)
 
 def when_pressed(event=None):
-    global x, tags, urls
+    global line, tags, urls
 
-    api = PushshiftAPI()
     try:
         si.get_data(tick_enter.get())
-    except:
-        text.insert('1.0', "Invalid Ticker")
+    except Exception:
+        text.insert('1.0', "Invalid Ticker\n")
         return
-        
-    date_to_use = datetime.now() - timedelta(days=3)
+
+    date_to_use = datetime.now() - timedelta(days=2)
     try:
         if date_enter.get() != "":
             date_to_use = datetime.strptime(date_enter.get(), '%m/%d/%Y')
-    except:
+    except Exception:
         date_to_use = datetime.now() - timedelta(days=3)
 
-    start = datetime(hour=1, month=date_to_use.month, year=date_to_use.year, day=date_to_use.day)
-    posts = list(api.search_submissions(q = str(tick_enter.get().upper()), after=start, subreddit='wallstreetbets', filter=['url', 'title',], limit=500))
+    api = PushshiftAPI()
 
+    start = int(datetime(hour=1, month=date_to_use.month, year=date_to_use.year, day=date_to_use.day).timestamp())
+    posts = api.search_submissions(q = str("$" + tick_enter.get().upper()), after=start, before = int((datetime.now() - timedelta(hours=2)).timestamp()), subreddit='wallstreetbets', filter=['url', 'title'], limit=500)
+    
     for post in posts:
         words = post.title.split()
         cashtags = list(set(filter(lambda word: word.lower().startswith('$'), words)))
         if len(cashtags) > 0:
             for cashtag in cashtags:
                 if ("$" + tick_enter.get().upper()) in cashtag:
-                    if "www.reddit.com" in post.url:
-                        #print(post.url)
-                        chrome_options = Options()
-                        chrome_options.add_argument("--headless")
-                        chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-                        browser = webdriver.Chrome(options=chrome_options)
+                    if "www.reddit.com" in post.url:                        
+                        browser = setup_chrome()
                         browser.get(post.url)
+
+                        # searching for this div tag <div class="Nt8TnDvJ2BsL8KWcFQKy5">Sorry, this post has been removed by the moderators of r/wallstreetbets.</div>
                         try:
                             browser.find_element_by_xpath("//div[@class='Nt8TnDvJ2BsL8KWcFQKy5']")
-                        except:                        
-                            urls["tag"+str(x)] = post.url
-                            text.insert('1.0', post.title + "\n")
-                            text.tag_add("tag"+str(x), '1.0', '1.end')
-                            text.tag_config("tag"+str(x), foreground='blue', underline=True)
-                            tag = "tag" + str(x)
-                            callback = lambda even, tag=tag: do(event, tag)
-                            text.tag_bind("tag"+str(x), '<Button-1>', callback)
-                            x = x + 1
+                        except Exception:                        
+                            line = add_link(event, post)
                         browser.quit()
                         break
-                    #class="Nt8TnDvJ2BsL8KWcFQKy5">Sorry, this post has been removed by the moderators of r/wallstreetbets.</div>
+                    
     
-x = 0
+
+#This defines layout, I haven't quite figured out how to make it all fit how I want.
 
 gui = tkinter.Tk()
-text = tkinter.Text(gui)
-#date = tkinter.Text(gui)
-gui.title("Recent Reddit Posts with Cashtags")
-tick = tkinter.Label(gui, text="Ticker:")
+main_frame = tkinter.Frame(gui)
+main_frame.pack()
+reddit_frame = tkinter.Frame(main_frame, height = 50)
 
-tick_enter = tkinter.Entry(gui, width = 25)
+text = tkinter.Text(reddit_frame)
+
+gui.title("Recent Reddit Posts with Cashtags")
+tick = tkinter.Label(reddit_frame, text="Ticker:")
+
+tick_enter = tkinter.Entry(reddit_frame, width = 25)
 tick_enter.bind('<Return>', when_pressed)
 
-dat = tkinter.Label(gui, text = "Date After (mm/dd/yyyy):")
+dat = tkinter.Label(reddit_frame, text = "Optional Date After (mm/dd/yyyy):")
 
-date_enter = tkinter.Entry(gui, width=25)
+date_enter = tkinter.Entry(reddit_frame, width=25)
 date_enter.bind('<Return>', when_pressed)
 
-enter_but = tkinter.Button(gui, text="Enter", command=when_pressed, height = 1, width = 10)
+enter_but = tkinter.Button(reddit_frame, text="Enter", command=when_pressed, height = 1, width = 10)
 
-text.pack(side='bottom')
-tick.pack(side='left')
-tick_enter.pack(side="left")
-dat.pack(side='left')
-date_enter.pack(side='left')
-enter_but.pack(side="left")
+reddit_frame.pack(anchor='nw')
+tick.pack(padx = 5, pady=5, side='top', anchor='nw')
+tick_enter.place(in_=tick, relx = 1.0, x = 5, rely = 0)
+dat.place(in_=tick_enter, relx = 1.0, x = 5, rely = 0)
+date_enter.place(in_ = dat, relx = 1.0, x=5, rely=0)
+enter_but.place(in_=date_enter, relx=1.0, x=5, rely=0)
+text.pack(side='top', pady=10, fill='none')
+
+gui.geometry("700x700")
 gui.mainloop()
